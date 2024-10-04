d30e9b9caf98a107b85196c5c231f64de2ba028d
Hoon <opunho118@naver.com>
2024-10-04 17:38:44 +0900

    초기 설정 테스트

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float moveX;
+    float moveY;
+    public float inputMagnitude { get; private set; }
+    public float decayRate = 2f;
+    public float moveSpeed = 10;
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+        transform.position += (Vector3)moveDirection * moveSpeed * Time.deltaTime;
+
+        if (moveDirection.magnitude > 0)
+        {
+            inputMagnitude = moveDirection.magnitude;
+        }
+        else
+        {
+            inputMagnitude = Mathf.Lerp(inputMagnitude, 0, decayRate * Time.deltaTime);
+            if (inputMagnitude < 0.8f)
+            {
+                inputMagnitude = 0;
+            }
+        }
+    }
+}
